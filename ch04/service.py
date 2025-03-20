b493719ec307cb75554bacace636394d65fad374
심현진 <hyunjin.dev143@gmail.com>
2025-03-20 16:00:28 +0900

    Feat: singleton 패턴 적용

--- ch04/service.py
+++ ch04/service.py
@@ -1,7 +1,7 @@
 #main
-from ch04.data.todo import find_all, insert_one
+#from ch04.data.todo import find_all, insert_one
 from ch04.models import Todo
-
+from ch04.singleton.todo import find_all, insert_one
 
 if __name__ == '__main__':
     print(find_all())
