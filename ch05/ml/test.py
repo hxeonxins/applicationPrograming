a69b6abe10439fee549e13e0fd1630bfd9c1d0b8
심현진 <hyunjin.dev143@gmail.com>
2025-04-03 16:10:57 +0900

    Feat: todo 카테고리 판별 AI 추가

--- ch05/ml/test.py
+++ ch05/ml/test.py
@@ -1,9 +1,22 @@
 import joblib
 
-vectorizer = joblib.load("vectorizer.joblib")
-model = joblib.load("todo_classifier.joblib")
+from ch05.ml.train_classifier import pipeline
+
+from transformers import pipeline
+
+# vectorizer = joblib.load("vectorizer.joblib")
+# model = joblib.load("todo_classifier.joblib")
+
+classifier = pipeline("zero-short-classification",
+                      model= "typeform/distilbert-base-uncased-mnli")
+CATEGORY = ['공부', '운동', '업무', '개인', '쇼핑']
 
 if __name__ == '__main__':
-    x = vectorizer.transform(['자바 공부하기'])
-    category = model.predict(x)[0]
-    print(category)
+    result = classifier('시험 공부하기', CATEGORY)
+    top_label = result['labels'][0]
+
+    # x = vectorizer.transform(['자바 공부하기'])
+    # category = model.predict(x)[0]
+    # print(category)
+
+# -> 서비스 계층에서 사용 가능!
